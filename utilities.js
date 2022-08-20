be2110e3045149f42523e63d721ac543d24be3ca
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-08-20 10:04:18 +0600

    added

---
+++ utilities.js
@@ -0,0 +1,48 @@
+function calculation() {
+    //Get salary 
+    const salaryField = document.getElementById('salary-field')
+    const salaryAmount = parseInt(salaryField.value)
+
+    //Get all Costs 
+    const foodField = document.getElementById('food-cost')
+    const foodCost = parseInt(foodField.value)
+
+    const rentField = document.getElementById('rent-cost')
+    const rentAmount = parseInt(rentField.value)
+
+    const clothField = document.getElementById('cloth-cost')
+    const clothCost = parseInt(clothField.value)
+
+    const totalExpense = foodCost + rentAmount + clothCost
+
+    //Set to total Expense
+    const totalExpenseField = document.getElementById('total-expense')
+    totalExpenseField.innerText = totalExpense
+    //Set to Balance
+    const balance = document.getElementById('minus-balance')
+    const minusBalance = salaryAmount - totalExpense
+    balance.innerText = minusBalance
+
+
+}
+
+function calculateSavings() {
+    //Get the balance Amount
+    const balanceText = document.getElementById('minus-balance')
+    const balance = Math.round(parseInt(balanceText.innerText))
+
+    //Get the saving percent amount
+    const savingField = document.getElementById('save-percent')
+    const savingPercent = parseInt(savingField.value)
+
+    //Now saving amount Calculation
+
+    // console.log((50 / 100) * 1000)
+    const percentAge = ((savingPercent / 100) * balance)
+
+
+    //Set it to saving Amount
+    const savingTotalField = document.getElementById('saving-amount')
+    savingTotalField.innerText = percentAge
+    return percentAge
+}
